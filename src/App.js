864428df5220ee18136835e6766be674a2977db4
Rishabh Bahukhandi <rishabhbahukhandi148@gmail.com>
2023-09-30 17:10:47 +0530

    Added the Delete and the complete button

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Task from './Task';
 
 function App() {
+
+  const [todoList, setTodoList] = useState([]);
+  const [newTask, setNewTask] = useState("");
+
+  const handleEvent = (event) => {
+    setNewTask(event.target.value);
+  }
+
+  const addTask = () => {
+    const task = {
+      id: todoList.length === 0 ? 1 : todoList[todoList.length - 1].id + 1,
+      taskName: newTask,
+      complete : false
+    }
+    setTodoList([...todoList, task]);
+  }
+  const deleteTask = (id) => {
+    setTodoList(todoList.filter((task) => id !== task.id));
+  }
+
+  const completeTask = (id) => {
+    setTodoList(
+      todoList.map((task) =>{
+        if(task.id === id){
+          return {...task , complete : true };
+        }
+        else{
+          return task;
+        }
+      })
+    )
+
+    }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <input type="text" onChange={handleEvent} />
+        <button onClick={addTask}>Add task</button>
+      </div>
+      <div>
+        {todoList.map((task) => {
+          return (
+            <Task className = "App" taskName={task.taskName}
+            id = {task.id} 
+            deleteTask={deleteTask}
+            completeTask = {completeTask}
+            complete = {task.complete}
+            />
+          )
+        })}
+      </div>
     </div>
   );
 }
---
+++ src/Task.js
@@ -0,0 +1,13 @@
+export const Task = (props) => {
+    return (<div
+        style={{backgroundColor: props.complete ? "green" : "white"}}
+    >
+        <h1>{props.id}</h1>
+        <h1>{props.taskName}</h1>
+        <button onClick={() => props.completeTask(props.id)}>Complete</button>
+        <button onClick={() => props.deleteTask(props.id)}>Delete</button>
+    </div>
+    );
+}
+
+export default Task;
