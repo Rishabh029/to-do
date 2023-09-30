13ec65ef8fe6e8b584ebdd7f3f43f6e10a3f6b12
Rishabh Bahukhandi <rishabhbahukhandi148@gmail.com>
2023-09-30 20:48:16 +0530

    Changes in UI

--- src/Task.js
+++ src/Task.js
@@ -6,6 +6,7 @@ export const Task = (props) => {
         <h1>{props.taskName}</h1>
         <button onClick={() => props.completeTask(props.id)}>Complete</button>
         <button onClick={() => props.deleteTask(props.id)}>Delete</button>
+        <p> A To Do list</p>
     </div>
     );
 }
